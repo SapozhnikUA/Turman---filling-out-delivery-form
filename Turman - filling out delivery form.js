fb194e7c06edd0c00762ef9a29ebb40b12e297ee
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2020-04-19 16:06:39 +0300

    Update

--- Turman - filling out delivery form.js
+++ Turman - filling out delivery form.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         Turman - filling out delivery form
 // @namespace    sapozhnik@gmail.com
-// @version      2020.04.19.001
+// @version      2020.04.19.002
 // @description  Turman - filling out delivery form (Ukraine)
 // @author       SapozhnikUA
 // @match        http://base.turman.ua/db/send_create
-// @include      http://base.turman.ua/db/send_create
-// @exclude      http://base.turman.ua/db/send_create*
 // @updateURL    https://github.com/SapozhnikUA/Turman---filling-out-delivery-form/raw/master/Turman%20-%20filling%20out%20delivery%20form.js
 // @downloadURL  https://github.com/SapozhnikUA/Turman---filling-out-delivery-form/raw/master/Turman%20-%20filling%20out%20delivery%20form.js
 // @supportURL   https://github.com/SapozhnikUA/Turman---filling-out-delivery-form/issues
