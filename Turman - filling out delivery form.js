6ef7c403dba65012d20fe4f18de81a66a9e4b56e
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2020-04-19 15:48:23 +0300

    Initial

---
+++ Turman - filling out delivery form.js
@@ -0,0 +1,15 @@
+  
+// ==UserScript==
+// @name         Turman - filling out delivery form
+// @namespace    sapozhnik@gmail.com
+// @version      2020.04.19.001
+// @description  Turman - filling out delivery form (Ukraine)
+// @author       SapozhnikUA
+// @match        http://base.turman.ua/db/send_create
+// @include      http://base.turman.ua/db/send_create
+// @exclude      http://base.turman.ua/db/send_create*
+// @updateURL    https://github.com/waze-ua/wme_minregion/raw/master/WME%20MinRegion.user.js
+// @downloadURL  https://github.com/waze-ua/wme_minregion/raw/master/WME%20MinRegion.user.js
+// @supportURL   https://github.com/waze-ua/wme_minregion/issues
+// @grant        none
+// ==/UserScript==
